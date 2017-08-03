72827cdb47fcc4f18704c5e72cb613867f0cfc90
Eduard Ramos <xseduard@gmail.com>
2017-08-03 01:54:19 -0500

    add errors field color template

--- resources/views/roles/fields.blade.php
+++ resources/views/roles/fields.blade.php
@@ -1,17 +1,17 @@
 <!-- Name Field -->
-<div class="form-group col-sm-6">
+<div class="form-group col-sm-6 has-feedback{{ $errors->has('name') ? ' has-error' : '' }}">
     {!! Form::label('name', 'llave interna') !!}
-    {!! Form::text('name', null, ['class' => 'form-control']) !!}
+    {!! Form::text('name', null, ['class' => 'form-control']) !!}    
 </div>
 
 <!-- Display Name Field -->
-<div class="form-group col-sm-6">
+<div class="form-group col-sm-6 has-feedback{{ $errors->has('display_name') ? ' has-error' : '' }}">
     {!! Form::label('display_name', 'Nombre Público') !!}
     {!! Form::text('display_name', null, ['class' => 'form-control']) !!}
 </div>
 
 <!-- Description Field -->
-<div class="form-group col-sm-12">
+<div class="form-group col-sm-12 has-feedback{{ $errors->has('description') ? ' has-error' : '' }}">
     {!! Form::label('description', 'Descripción') !!}
     {!! Form::textarea('description', null, ['class' => 'form-control']) !!}
 </div>
